14601d47859f35f515d57aab003e1282168ad180
hillaryoyaro <hillaryoyaroh@gmail.com>
2023-03-14 14:16:38 +0300

    ---Our store Update--

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const BreadCrumb = () => {
+const BreadCrumb = (props) => {
+    const {title} = props;
   return (
-    <div><h1>BreadCrumb</h1></div>
+    <div className="bread-crumb py-4">
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-12">
+                    <p className="text-center mb-0">
+                        <Link to="/" className="text-dark">
+                            Home &nbsp;
+                        </Link>
+                        / {title}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
   );
 }
 
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -7,6 +7,56 @@ function OurStore() {
     <>
     <Meta title={"Our Store"}/>
     <BreadCrumb title ="Our Store"/>
+    <div className="store-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-3">
+                    <div className="filter-card">
+                        <h3 className="filter-title">Shop By Categories</h3>
+                        <div>
+                            <ul className="ps-0">
+                                <li>Watch</li>
+                                <li>Tv</li>
+                                <li>Camera</li>
+                                <li>Laptope</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="filter-card">
+                        <h3 className="filter-title">Filter By</h3>
+                        <div>
+                            <h5 className="sub-title">Availability</h5>
+                            <div className="check-box">
+                            <input class="form-check-input" 
+                                type="checkbox" 
+                                id="" 
+                                value=""/>
+                                <label class="form-check-label" for="">
+                                    In Stock{1}
+                                </label>
+                            </div>
+                            <div className="check-box">
+                                <input class="form-check-input" 
+                                type="checkbox" 
+                                id="" 
+                                value=""/>
+                                <label class="form-check-label" for="">
+                                    Out of stock{0}
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="filter-card">
+                        <h3 className="filter-title">Product Tags</h3>
+                    </div>
+                    <div className="filter-card">
+                        <h3 className="filter-title">Random Product</h3>
+                    </div>
+                </div>
+                <div className="col-9"></div>
+            </div>
+        </div>
+    </div>
     </>
   );
 };
